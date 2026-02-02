789e791c11ec4b5d2a5a163864c3966e2e34694a
Jakub Stolarczyk <89207@student.pb.edu.pl>
2026-02-02 21:09:49 +0100

    prompts with templates

--- OldStyleReactAgents.py
+++ OldStyleReactAgents.py
@@ -1,21 +1,38 @@
 from dotenv import load_dotenv
+from langchain_core import output_parsers
 load_dotenv()
 
 from langchain_classic import hub
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-3.5-turbo") 
 react_prompt = hub.pull("hwchase17/react")
+
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructuons = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input","agent_scratchpad","tool_names"]
+).partial(format_instruction=output_parser.get_format_instructions())
+
 agent = create_react_agent(
     llm = llm,
     tools = tools,
-    prompt = react_prompt,
+    prompt = react_prompt_with_format_instructuons,
 )
+
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+extract_output = RunnableLambda(lambda x: x["output"])
+
 chain = agent_executor
 
 def main():         
